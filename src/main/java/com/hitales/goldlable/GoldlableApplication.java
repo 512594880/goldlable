3f34b112f0abbf16d6b23ddda1e47cd5c93876c0
zh <hao.zhuo@hitales.com.cn>
2018-09-04 14:38:41 +0800

    第一版提交

---
+++ src/main/java/com/hitales/goldlable/GoldlableApplication.java
@@ -0,0 +1,12 @@
+package com.hitales.goldlable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldlableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldlableApplication.class, args);
+	}
+}
