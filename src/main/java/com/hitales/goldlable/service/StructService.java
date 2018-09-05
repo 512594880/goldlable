8f12420b51e38560f5579462ec4ea2dcc5ed9373
XiWangWy <413933747@qq.com>
2018-09-05 16:30:27 +0800

    ss

--- src/main/java/com/hitales/goldlable/service/StructService.java
+++ src/main/java/com/hitales/goldlable/service/StructService.java
@@ -2,13 +2,14 @@ package com.hitales.goldlable.service;
 
 import com.alibaba.fastjson.JSONObject;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by wangxi on 18/9/5.
  */
-
+@Service
 @FeignClient(url = "${structUrl}",name="struct-client")
 public interface StructService {
     @PostMapping(value = "/show/elmaterial/one")
